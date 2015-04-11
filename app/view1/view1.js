b86dcd86c41f7a93b5051f8076387650b4faf330
Steve Duan <xdduan@gmail.com>
2015-04-10 21:47:31 -0700

    Added D3 bar chart

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,6 +9,26 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', function ($scope) {
+    $scope.myData = [10,20,30,40,60];
+})
 
-}]);
+.directive('barsChart', function ($parse) {
+  return {
+      restrict: 'E',
+      //we don't want to overwrite our directive declaration
+      //in the HTML mark-up
+      replace: false,
+      //scope: {data: '=chartData'},
+      link: function (scope, element, attrs) {
+        var data = attrs.chartData.split(',');
+        var chart = d3.select(element[0]);
+         chart.append("div").attr("class", "chart")
+          .selectAll('div')
+          .data(data).enter().append("div")
+          .transition().ease("elastic")
+          .style("width", function(d) { return d + "%"; })
+          .text(function(d) { return d + "%"; });
+      } 
+   };
+});
