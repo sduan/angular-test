6d10dd83d0ee25b0abdac8e771d8c06392eac118
Steve Duan <xdduan@gmail.com>
2015-04-12 16:25:09 -0700

    Added Highcharts directives

--- app/app.js
+++ app/app.js
@@ -3,10 +3,12 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
+  'highcharts-ng',
   'myApp.mandelbrot',
   'myApp.view1',
   'myApp.view2',
   'myApp.forcedirected',
+  'myApp.highchart',
   'myApp.version'
 ]).
 config(['$routeProvider', function($routeProvider) {
---
+++ app/highchart/highchart_test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('myApp.highchart module', function() {
+
+  beforeEach(module('myApp.highchart'));
+
+  describe('highchart controller', function(){
+
+    it('should ....', inject(function($controller) {
+      //spec body
+      var highchartCtrl = $controller('HighchartCtrl');
+      expect(highchartCtrl).toBeDefined();
+    }));
+
+  });
+});
